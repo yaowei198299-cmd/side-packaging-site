7f4a95108b275c24b6081644ad1cb44a24bfb4a8
yaowei198299-cmd <yaowei198299@gmail.com>
2026-05-15 11:06:09 +0800

    fix: add missing InquiryForm imports in blog pages

--- pages/blog/2026-haptic-luxury-rigid-box-trends.tsx
+++ pages/blog/2026-haptic-luxury-rigid-box-trends.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { triggerInquiryModal } from '../../components/InquiryModal';
+import InquiryForm from '../../components/InquiryForm';
 
 const BlogPage = () => {
   return (
