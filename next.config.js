79ac68861e959ce72b16bb2736b7045756a2ea6e
yaowei198299-cmd <yaowei198299@gmail.com>
2026-06-26 09:12:08 +0800

    perf: comprehensive WebP migration and code-level refactor

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-}
-
-module.exports = nextConfig
--- pages/test-deploy.tsx
+++
@@ -1,6 +0,0 @@
-﻿import React from 'react';
-const TestPage = () => {
-  return <div>Test Page Online</div>;
-};
-export default TestPage;
-
